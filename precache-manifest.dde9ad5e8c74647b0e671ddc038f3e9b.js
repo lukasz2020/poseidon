2c1870944c6b9bd36d1c664a7ce2d90e83d832ef
lukasz2020 <lukl1@wp.pl>
2020-06-16 23:46:13 +0200

    Updates

---
+++ precache-manifest.dde9ad5e8c74647b0e671ddc038f3e9b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef0a8f76d22111444efff71fa5561d08",
+    "url": "/poseidon/index.html"
+  },
+  {
+    "revision": "05098ea1a8b5ff27b40d",
+    "url": "/poseidon/static/css/main.2990201e.chunk.css"
+  },
+  {
+    "revision": "6c133a72c3eacc32efe3",
+    "url": "/poseidon/static/js/2.62db172f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/poseidon/static/js/2.62db172f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05098ea1a8b5ff27b40d",
+    "url": "/poseidon/static/js/main.1e5fb821.chunk.js"
+  },
+  {
+    "revision": "020df57122d9ae851a9b",
+    "url": "/poseidon/static/js/runtime-main.d8be8ac4.js"
+  }
+]);
